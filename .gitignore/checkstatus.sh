bb736c2a7b010b25195340a5ae6a9e54b7d3e25d
naveenkashyapdv <d.naveenkashyap@gmail.com>
2017-09-27 16:32:13 -0700

    Delete checkstatus.sh

--- .gitignore/checkstatus.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-filename = 'urls.txt'
-while read url
-do
-    urlstatus=$(curl -Is "$url" | head -n 1 )
-	urlredirect=$(curl -w "%{url_effective}\n" -I -L -s -S "$url" -o /dev/null )
-    echo "$urlstatus + $urlredirect" >> urlstatus.txt 
-	
-done < $1
